d639cf6fcd073aeecd1bd6acb4dcdbabb8f1b80a
Anupam <anubhatt10101@gmail.com>
2024-08-25 22:23:57 +0530

    Add files via upload

---
+++ #PROJECT-02-who_would_pay_th_bill.py
@@ -0,0 +1,6 @@
+#PROJECT-02-who_would_pay_th_bill
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+ind = random.randint(0,4)
+name = friends[ind]
+print ("\n\n\nThe bill would be paid by "+name+"! WooHoooo! \n\n\n",)
